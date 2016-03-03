10917fc5dd4ce2d909677055e2d01d9d9514d1f9
Pietro <pulsarpietro@posteo.net>
2016-03-03 18:41:47 +0000

    The project is almost complete, missing steps :
    1 - Line justification does not work as intended
    2 - Unit tests

--- kwic.scala
+++ kwic.scala
@@ -1,4 +1,4 @@
-import scala.collection.mutable.{Map, HashMap,ListBuffer}
+import scala.collection.mutable.{Map, LinkedHashMap,ListBuffer}
 import scala.collection.immutable.StringOps
 import scala.util.control.Exception._
 
@@ -16,7 +16,7 @@ class KWICFile(filename: String) {
   val fileName  = filename
   val fileSource = scala.io.Source.fromFile(filename)
   val lines     = fileSource.getLines.toArray
-  val kwicMap   = new HashMap[String, ListBuffer[Record]]
+  val kwicMap   = new LinkedHashMap[String, ListBuffer[Record]]
 
   fileSource.close()
   
@@ -24,11 +24,33 @@ class KWICFile(filename: String) {
 
     // Transform every line in a pair of (line, content) and filter out
     // the line not containing the specific keyword
-    val filtered = this.lines.zipWithIndex.filter(_._1.contains(keyword))
-    val list     = ListBuffer[Record]()
+    val filtered     = this.lines.zipWithIndex.filter(_._1.contains(keyword))
+    val list         = ListBuffer[Record]()
+    val unwantedChar = """[0-9]|\.|;|,|"|:""".r
 
-    for (line <- filtered)
-      list += new Record(line._2 + 1, KWICFile.transformContent(line._1))
+    for (line <- filtered) {
+      // Strip all the undesired punctuation and digits
+      var transformedLine = unwantedChar.replaceAllIn(line._1.toLowerCase, "")
+      println("Line "  + transformedLine)
+      val keywordIndex = transformedLine.indexOf(keyword) - 1
+      println("Index " + keywordIndex)
+      var stringLeft = transformedLine.substring(0, scala.math.max(0, keywordIndex))
+      println("Left two " + stringLeft)
+      if (stringLeft.length > 30)
+        stringLeft =  stringLeft.substring(stringLeft.length - 30 , 30)
+      var stringRight = transformedLine.substring(keywordIndex + keyword.length + 1, transformedLine.length)
+      if (stringRight.length > 30)
+        stringRight = stringRight.substring(0, scala.math.min(stringRight.length, 29))
+
+      println("String left " + stringLeft)
+      println("String right " + stringRight)
+      println("String keyword " + keyword)
+      // Left and right justification
+      val justifiedLeft  = " " * (30 - stringLeft.length)
+      val justifiedRight = " " * (30 - stringRight.length)
+      transformedLine = justifiedLeft + stringLeft + " " + keyword + stringRight + justifiedRight
+      list += new Record(line._2 + 1, transformedLine)
+    }
     this.kwicMap.+=(keyword -> list)
     list
   }
@@ -38,9 +60,7 @@ class KWICFile(filename: String) {
   def formatOutput() : String = {
     var ret : String = this.fileName + "\n"
     for ((k,v) <- this.kwicMap) {
-      println("Keyword " + k)
       v.foreach( (item) => {
-        println(item)
         ret += item.rindex + " "
         ret += item.rcontent + "\n"
       })
@@ -71,17 +91,6 @@ object KWICFile {
    * 2) Lowercase
    * 3) Remove digits and punctuation except apostrophes
    */
-  private def transformContent(content : String) : String = {
-    val _ret = new StringOps(content)
-    _ret.toLowerCase().filter(
-      (c) => {
-        c match {
-          case digitRE(_*)       => false
-          case punctuationRE(_*) => false
-          case _                 => true
-        }
-      })
-  }
 }
 
 object KWICUserInteraction {
@@ -94,6 +103,8 @@ object KWICUserInteraction {
       val stopWords : Array[String]    = fileStream.getLines.toArray
 
       fileStream.close()
+      scala.util.Sorting.quickSort(stopWords)
+
       println ("File stop-words.txt loaded successfully : " +
         stopWords.length +
         " words found")
@@ -107,8 +118,10 @@ object KWICUserInteraction {
           throw new AllDoneException()
         try {
           val kwicFile = new KWICFile(path)
-          for (keyword <- stopWords)
+          for (keyword <- stopWords) {
+            println("Examining keyword " + keyword)
             kwicFile.keywordInContext(keyword)
+          }
           kwicFile.writeToFile(fileWriter)
           println(kwicFile.formatOutput)
           fileWriter.flush()
